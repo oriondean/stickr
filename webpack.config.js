5e2c737c92f5018800340f3040f20e54772d9eaf
Dean Kerr <dkerr@scottlogic.co.uk>
2016-04-07 11:12:53 +0100

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var scripts = path.resolve(__dirname, "app/scripts");
+
+module.exports = {
+    entry: [
+        './app/scripts/index.js'
+    ],
+    output: {
+        path: './build',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: scripts
+            }
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: scripts,
+                query: { presets: ['es2015', 'react'] }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html',
+                include: scripts
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass'],
+                include: scripts
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: path.resolve(__dirname, "app/styles")
+            },
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        ]
+    }
+};
