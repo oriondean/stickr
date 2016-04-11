9ba1ec9f7191e2f05e76655abfad6f22e4b91fc3
Dean Kerr <dean_k@live.co.uk>
2016-04-11 22:46:10 +0100

    Updated app container to use sticker card component

--- app/scripts/containers/App.js
+++ app/scripts/containers/App.js
@@ -1,3 +1,4 @@
+import * as ActionCreators from '../actions/index';
 import { connect } from 'react-redux';
 import React from 'react';
 
@@ -5,17 +6,32 @@ import './app.scss';
 
 // Components
 import Header from '../components/header/header';
+import StickerCard from '../components/sticker-card/sticker-card';
 
 class App extends React.Component {
     render() {
+        const sticker = this.props.stickers.get('15');
+
         return <div className="app">
             <Header />
             <h1>Hello World</h1>
+            <StickerCard name={sticker.get('name')} count={sticker.get('count')} number={sticker.get('number')}
+                         increment={this.props.incrementStickerCount} decrement={this.props.decrementStickerCount} />
         </div>
     }
 }
 
-export default connect(
-    () => { return {}; },
-    () => { return {}; }
-)(App);
+const mapStateToProps = state => {
+    return {
+        stickers: state.stickers
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        incrementStickerCount: stickerNumber => dispatch(ActionCreators.incrementStickerCount(stickerNumber)),
+        decrementStickerCount: stickerNumber => dispatch(ActionCreators.decrementStickerCount(stickerNumber))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
