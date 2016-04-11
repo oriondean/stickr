fd0ffa55587288466f8a2794a133b19c60e0f4db
Dean Kerr <dean_k@live.co.uk>
2016-04-11 22:45:56 +0100

    Updated sticker reducer to handle increment and decrement

--- app/scripts/reducers/stickers.js
+++ app/scripts/reducers/stickers.js
@@ -1,9 +1,27 @@
 import Immutable from 'immutable';
+import {STICKER_ACTIONS} from '../constants/action-types';
 
-const initialState = Immutable.Map();
+const initialState = Immutable.fromJS({
+    '15': { number: '15', count: 0, name: 'Jamie Vardy' },
+    '17': { number: '17', count: 1, name: 'Wade Ronney' }
+});
 
 export default function reduce(state = initialState, action) {
     switch(action.type) {
+        case STICKER_ACTIONS.INCREMENT_COUNT:
+        {
+            const sticker = state.get(action.stickerNumber);
+            return state.set(action.stickerNumber, sticker.set('count', sticker.get('count') + 1));
+        }
+        case STICKER_ACTIONS.DECREMENT_COUNT: {
+            const sticker = state.get(action.stickerNumber);
+
+            if(sticker.get('count') === 0) {
+                throw new Error('A sticker cannot have a count less than zero');
+            }
+
+            return state.set(action.stickerNumber, sticker.set('count', sticker.get('count') - 1));
+        }
         default:
             return state;
     }
