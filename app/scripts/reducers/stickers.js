0559e3eab115cacc91bbe5e761872019f0648b87
Dean Kerr <dean_k@live.co.uk>
2016-04-16 21:25:35 +0100

    Updated stickers reducer to handle new sets of stickers

--- app/scripts/reducers/stickers.js
+++ app/scripts/reducers/stickers.js
@@ -1,19 +1,15 @@
 import Immutable from 'immutable';
 import {STICKER_ACTIONS} from '../constants/action-types';
 
-const initialState = Immutable.fromJS({
-    '15': { number: '15', count: 0, name: 'Jamie Vardy' },
-    '17': { number: '17', count: 1, name: 'Wade Ronney' },
-    '19': { number: '19', count: 3, name: 'Puke Shaw' },
-    '21': { number: '21', count: 0, name: 'Adam Lollana' },
-    '25': { number: '23', count: 2, name: 'Nod Flonders' },
-    '27': { number: '27', count: 0, name: 'Jake Bugg' }
-});
+// List of stickers, mapped by their set
+const initialState = Immutable.Map();
 
 export default function reduce(state = initialState, action) {
     switch(action.type) {
-        case STICKER_ACTIONS.INCREMENT_COUNT:
-        {
+        case STICKER_ACTIONS.UPDATE_STICKERS: {
+            return state.set(String(action.setId), Immutable.fromJS(action.stickers));
+        }
+        case STICKER_ACTIONS.INCREMENT_COUNT: {
             const sticker = state.get(action.stickerNumber);
             return state.set(action.stickerNumber, sticker.set('count', sticker.get('count') + 1));
         }
