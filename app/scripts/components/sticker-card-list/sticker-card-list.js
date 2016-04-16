df01c4756c8b8d34b0d40fdcbe45828067bcadec
Dean Kerr <dean_k@live.co.uk>
2016-04-16 21:26:56 +0100

    Updated sticker card list to handle new sticker map structure

--- app/scripts/components/sticker-card-list/sticker-card-list.js
+++ app/scripts/components/sticker-card-list/sticker-card-list.js
@@ -6,15 +6,28 @@ import './sticker-card-list.scss';
 class StickerCardList extends Component {
     render() {
         const stickers = this.props.stickers.map(sticker => {
-            return <StickerCard name={sticker.get('name')}
-                                count={sticker.get('count')}
-                                number={sticker.get('number')}
-                                key={sticker.get('name')}
+            const item = sticker.get('item');
+            return <StickerCard name={StickerCardList.formatName(item.get('name'))}
+                                count={item.get('count') || 0}
+                                number={item.get('number')}
+                                key={item.get('name')}
                                 increment={this.props.increment}
                                 decrement={this.props.decrement} />
         }).toArray();
 
         return <div className="sticker-list">{stickers}</div>;
+    }
+
+    static formatName(stickerName) {
+        const names = stickerName.trim().split('/');
+
+        return names.map(name => {
+            const subNames = name.split(' ');
+
+            return subNames.map(subName => {
+                return subName.substr(0, 1).toUpperCase() + subName.substr(1);
+            }).join(' ');
+        }).join('/');
     }
 }
 
