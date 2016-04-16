55e9cc09f51a3842ae77fd66dc8f3f6f0d163ad0
Dean Kerr <dean_k@live.co.uk>
2016-04-16 16:38:03 +0100

    Updated header component to show session message

--- app/scripts/components/header/header.js
+++ app/scripts/components/header/header.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react'
 
 class Header extends Component {
     render() {
-        return (
-            <nav className="navbar navbar-fixed-top navbar-inverse">
-                <div className="container">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="#">Stickr</a>
-                    </div>
+        let sessionMessage = null;
+
+        if(this.props.isLoggedIn && this.props.user) {
+            sessionMessage = <p className="navbar-text">
+                Signed in as {this.props.user.get('name')}
+            </p>;
+        }
+
+        return <nav className="navbar navbar-fixed-top navbar-inverse">
+            <div className="container">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#">Stickr</a>
+                    {sessionMessage}
                 </div>
-            </nav>
-        )
+            </div>
+        </nav>
     }
 }
 
-export default Header
+Header.propTypes = {
+    isLoggedIn: React.PropTypes.bool.isRequired,
+    user: React.PropTypes.object
+};
+
+export default Header;
