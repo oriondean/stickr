39bca15bd05ee2b28fb4eb98c7d82e9bfce30aee
Dean Kerr <dean_k@live.co.uk>
2016-04-16 21:25:17 +0100

    Added getStickersBySetId action

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -1,4 +1,5 @@
 import { push } from 'react-router-redux';
+import * as API from '../constants/api';
 import {STICKER_ACTIONS, AUTH_ACTIONS} from '../constants/action-types';
 
 export const incrementStickerCount = stickerNumber => {
@@ -25,7 +26,7 @@ export const loginSuccess = user => {
             type: AUTH_ACTIONS.LOGIN_SUCCESS,
             user
         });
-        dispatch(push('home'));
+        dispatch(push('/home'));
     };
 };
 
@@ -35,5 +36,27 @@ export const loginFailure = error => {
             type: AUTH_ACTIONS.LOGIN_FAILURE,
             error
         });
+    };
+};
+
+export const getStickersBySetId = setId => {
+    return dispatch => {
+        const request = new XMLHttpRequest();
+        request.withCredentials = true;
+        request.open(API.GET_USER_SET_ITEMS.method, API.GET_USER_SET_ITEMS.getAddress(setId), true);
+        request.setRequestHeader('Content-Type', 'application/json');
+
+        request.onload = () => {
+            dispatch({
+                type: STICKER_ACTIONS.UPDATE_STICKERS,
+                setId,
+                stickers: JSON.parse(request.response).results
+            });
+        };
+
+        // TODO: nicely handle failure to retrieve stickers
+        request.onerror = () => { console.error('Failed to retrieve stickers for set ' + setId); };
+
+        request.send();
     };
 };
